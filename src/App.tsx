ae61a2ed7b5413ecf170f3f97d2d7a3048c7ea6b
Mészáros Róbert <mrobert3456@gmail.com>
2024-06-29 10:25:39 +0200

    Added a Not found page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import MainLayout from "./_components/layout/MainLayout";
 import CharactersList from "./_components/characters/CharactersList";
 import { Profile } from "./_components/profile/Profile";
+import Notfound from "./_components/notfound/Notfound";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<CharactersList />} />
         <Route path="profile/:id" element={<Profile />} />
+        <Route path="*" element={<Notfound />} />
       </Route>
     </Routes>
   );
