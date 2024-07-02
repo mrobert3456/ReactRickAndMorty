01ce58370f7c2dc5fb847864993b88f7e7b556ba
Mészáros Róbert <mrobert3456@gmail.com>
2024-07-02 12:34:43 +0200

    created unit tests for episodeUtils

---
+++ src/tests/episodeUtils.test.ts
@@ -0,0 +1,15 @@
+import { getEpisodeNumberIdFromUrl } from "../_components/utils/episodeUtils";
+import { describe, expect } from "@jest/globals";
+describe("getEpisodeNumberIdFromUrl tests", () => {
+  it("return the number from the end of the URL", () => {
+    const result = getEpisodeNumberIdFromUrl("http://test.com/episode/123");
+    expect(result).toBe(123);
+  });
+
+  it("return -1 if there is no number at the end of the URL", () => {
+    const result = getEpisodeNumberIdFromUrl(
+      "http://test.com/episode/123/something"
+    );
+    expect(result).toBe(-1);
+  });
+});
